8e8213b98a8215cc229e702f8d243721354a8522
ShyLP <46283518+ShyLP@users.noreply.github.com>
2021-03-22 21:11:11 +0700

    Update bai tap 1.cpp

--- bai tap 1.cpp
+++ bai tap 1.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+int ucln(int a, int b)
+{
+    int r;
+    while (a % b != 0)
+    {
+    r = a % b;
+    a = b;
+    b = r;
+    }
+    return b;
+}
+int main()
+{
+    long a, b;
+    cin >> a >> b;
+    cout << "ucln = " << ucln(a, b) << endl;
+    cout << "bcnn = " << a * b / ucln(a, b) << endl;
+    return 0;
+}
 
